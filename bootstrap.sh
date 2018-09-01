b6c73663c5f8f4fd1fc42e5c5ab3e863ac00bb26
Sugeng Tigefa <tigefa@gmail.com>
2018-09-02 03:49:02 +0700

    uncomment task run :up:

--- bootstrap.sh
+++ bootstrap.sh
@@ -58,8 +58,8 @@ service unattended-upgrades restart
 
 # install apparmor
 $minimal_apt_get_install apparmor apparmor-profiles apparmor-utils
-sed -i.bak 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="\1 apparmor=1 security=apparmor"/' /etc/default/grub#
-update-grub
+#sed -i.bak 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="\1 apparmor=1 security=apparmor"/' /etc/default/grub#
+#update-grub
 
 # install ntp (tlsdate is no longer available in Debian stable)
 $minimal_apt_get_install ntp
